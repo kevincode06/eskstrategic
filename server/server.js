8d08da0f230f81d80bd0f91acb8964d655151740
Kevin Malou <maloukevin6@gmail.com>
2026-04-17 11:24:54 +0200

    push

--- server/server.js
+++ server/server.js
@@ -11,10 +11,13 @@ app.use(cors());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true,
+  secure: false, // false for 587, true for 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // needed for some hosting providers
   },
 });
 
